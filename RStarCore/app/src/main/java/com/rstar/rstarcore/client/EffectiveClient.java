2ed52de8d19abf4616393e4ed55405abe2400031
zqtOlive <zqt_olive@sohu.com>
2019-04-15 12:27:19 +0800

    [RStarCore]Add code.

---
+++ RStarCore/app/src/main/java/com/rstar/rstarcore/client/EffectiveClient.java
@@ -0,0 +1,16 @@
+package com.rstar.rstarcore.client;
+
+/**
+ * @Package: com.rstar.rstarcore.client
+ * @ClassName: EffectiveClient
+ * @Description:
+ * @Author: 庆涛
+ * @Email: zqt_olive@sina.com
+ * @CreateDate: 2019/4/15 12:11
+ * @UpdateUser:
+ * @UpdateDate: 2019/4/15 12:11
+ * @UpdateRemark:
+ * @Version: 1.0
+ */
+class EffectiveClient implements IClient {
+}
